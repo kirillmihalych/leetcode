d9c43d635667e93a391d94234ba827ffecc6b824
KIRILL F <tiltlessman@gmail.com>
2023-07-17 14:27:16 +0300

    container with most water

---
+++ Two Pointers/Container with most water.js
@@ -0,0 +1,23 @@
+// 2 Pointers
+// https://leetcode.com/problems/container-with-most-water/
+const maxArea = function (height) {
+  let maxAmount = 0
+  let left = 0
+  let right = height.length - 1
+
+  while (left < right) {
+    let leftHeight = height[left]
+    let rightHeight = height[right]
+
+    let volume = Math.min(leftHeight, rightHeight) * Math.abs(left - right)
+    maxAmount = Math.max(maxAmount, volume)
+
+    const isRightGreater = rightHeight > leftHeight
+    if (isRightGreater) left++
+
+    const isLeftGreater = leftHeight >= rightHeight
+    if (isLeftGreater) right--
+  }
+
+  return maxAmount
+}
