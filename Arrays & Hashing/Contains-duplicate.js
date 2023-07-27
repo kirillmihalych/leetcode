6cf35c85638be56a7d61f19338da6a154b0eea01
KIRILL F <tiltlessman@gmail.com>
2023-07-27 10:14:33 +0300

    [refresh] containss-duplicate

--- Arrays & Hashing/Contains-duplicate.js
+++ Arrays & Hashing/Contains-duplicate.js
@@ -1,4 +1,5 @@
 // Set solution
+// Time O(n) || Space O(n)
 // https://leetcode.com/problems/contains-duplicate/
 
 var containsDuplicate = function (nums) {
@@ -8,4 +9,28 @@ var containsDuplicate = function (nums) {
 
   const set = new Set(nums)
   return set.size < nums.length ? true : false
+}
+
+// Brute force
+// Time O(N^2) | Space O(1)
+var containsDuplicate = (nums) => {
+  for (let right = 0; right < nums.length; right++) {
+    for (let left = 0; left < right; left++) {
+      const isDuplicate = nums[left] === nums[right]
+      if (isDuplicate) return true
+    }
+  }
+
+  return false
+}
+
+// Set - Early exit
+// Time O(n) || Space O(n)
+const containsDuplicate = (nums, setOfNums = new Set()) => {
+  for (const num of nums) {
+    if (setOfNums.has(num)) return true
+    setOfNums.add(num)
+  }
+
+  return false
 }
