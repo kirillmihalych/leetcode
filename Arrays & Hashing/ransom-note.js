a103cb0c951ac029e2f31479e9e596dddf979a6b
KIRILL F <tiltlessman@gmail.com>
2023-08-13 10:28:26 +0300

    ransom-note

---
+++ Arrays & Hashing/ransom-note.js
@@ -0,0 +1,21 @@
+// Hashing
+// Time O(n) || Space O(n)
+// https://leetcode.com/problems/ransom-note/
+
+const canConstruct = function (ransomNote, magazine) {
+  const noteMap = getMap(ransomNote)
+  const magazineMap = getMap(magazine)
+
+  for (const [key, value] of noteMap) {
+    if (!(magazineMap.has(key) && magazineMap.get(key) >= value)) return false
+  }
+
+  return true
+}
+
+const getMap = (str, map = new Map()) => {
+  for (let i = 0; i < str.length; i++) {
+    map.set(str[i], map.get(str[i]) + 1 || 1)
+  }
+  return map
+}
