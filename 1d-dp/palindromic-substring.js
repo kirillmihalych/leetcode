a7b81e043b4a2d926a4186bb6bccf82bb2f4ea67
KIRILL F <tiltlessman@gmail.com>
2023-08-22 11:37:54 +0300

    palindromic-substring-expand-around-center

--- 1d-dp/palindromic-substring.js
+++ 1d-dp/palindromic-substring.js
@@ -24,3 +24,30 @@ const isPalindrome = (left, right, s) => {
 
   return true
 }
+
+// Expand around center
+// Time O(N ^ 2) || Space ??
+// https://leetcode.com/problems/palindromic-substrings
+
+const countSubstringsTwo = function (s) {
+  let count = 0
+  for (let i = 0; i < s.length; i++) {
+    // around one char
+    let amount1 = getPalindromeCount(s, i, i)
+    // around two chars
+    let amount2 = getPalindromeCount(s, i, i + 1)
+    count = count + amount1 + amount2
+  }
+
+  return count
+}
+
+const getPalindromeCount = (str, i, j, count = 0) => {
+  while (i >= 0 && j <= str.length - 1 && str[i] === str[j]) {
+    count++
+    i -= 1
+    j += 1
+  }
+
+  return count
+}
