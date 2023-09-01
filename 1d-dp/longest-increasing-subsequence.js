78b2ce722e38787176f0d67607045765468b5e81
KIRILL F <tiltlessman@gmail.com>
2023-09-01 12:35:42 +0300

    longest-increasing-subsequence

---
+++ 1d-dp/longest-increasing-subsequence.js
@@ -0,0 +1,17 @@
+// dp memo
+// Time ?? || Space ??
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+function lengthOfLIS(nums) {
+  if (!nums.length) return 0
+
+  let dp = new Array(nums.length).fill(1)
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1)
+    }
+  }
+
+  return Math.max(...dp)
+}
