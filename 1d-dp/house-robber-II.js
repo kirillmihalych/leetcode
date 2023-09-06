dea3e00dd19dbf6dd1435d812c0aba887cba45f3
KIRILL F <tiltlessman@gmail.com>
2023-09-06 14:27:17 +0300

    [refresh] house-robber-ii

--- 1d-dp/house-robber-II.js
+++ 1d-dp/house-robber-II.js
@@ -1,31 +1,33 @@
-// dp
-// Time ?? || Space ??
-//leetcode.com/problems/house-robber-ii/
-https: var rob = function (nums, i = 0, memo = new Map()) {
+// dp - memo
+// Time O(n * 2) || Space O(n * 2)
+// https://leetcode.com/problems/house-robber-ii/
+
+const rob = function (nums) {
   if (nums.length === 1) return nums[0]
 
-  const helper = (nums, i = 0, memo = new Map()) => {
-    const isBaseCase = i >= nums.length
+  const recursive = (nums, i, memo = new Map()) => {
+    const isBaseCase = i < 0
     if (isBaseCase) return 0
 
     if (memo.has(i)) return memo.get(i)
 
-    const [next, nextNext] = [i + 1, i + 2]
-    const oneStep = helper(nums, next, memo)
-    const twoStep = helper(nums, nextNext, memo) + nums[i]
-
+    const oneStep = recursive(nums, i - 1, memo)
+    const twoSteps = recursive(nums, i - 2, memo) + nums[i]
     memo.set(
       i,
-      memo.get(i) + Math.max(oneStep, twoStep) || Math.max(oneStep, twoStep)
+      memo.get(i) + Math.max(oneStep, twoSteps) || Math.max(oneStep, twoSteps)
     )
+
     return memo.get(i)
   }
 
   let arr = [...nums]
   arr.splice(arr.length - 1)
-  console.log(arr)
   let arr2 = [...nums]
   arr2.splice(0, 1)
 
-  return Math.max(helper(arr, 0), helper(arr2, 0))
+  return Math.max(
+    recursive(arr, nums.length - 2),
+    recursive(arr2, nums.length - 2)
+  )
 }
