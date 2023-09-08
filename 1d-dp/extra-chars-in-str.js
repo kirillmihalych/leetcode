88e1a3a50714e354fec9a30e6524bdd78ab1c9e7
KIRILL F <tiltlessman@gmail.com>
2023-09-08 12:32:14 +0300

    [refresh] word-break and extra-charachters-in-string with reusable pattern

--- 1d-dp/extra-chars-in-str.js
+++ 1d-dp/extra-chars-in-str.js
@@ -1,5 +1,5 @@
-// dp
-// Time ?? || Space ??
+// dp meo without recursion
+// Time O(n ^ 2) || Space O(n)
 // https://leetcode.com/problems/extra-characters-in-a-string/
 
 function minExtraChar(s, dictionary) {
@@ -10,25 +10,24 @@ function minExtraChar(s, dictionary) {
   const n = s.length
 
   // Initialize an array to store the minimum extra characters needed for substrings of s.
-  const dp = new Array(n + 1).fill(0)
+  const memo = new Map()
 
   for (let i = 1; i <= n; i++) {
     // Start by assuming that adding one character is needed.
-    dp[i] = dp[i - 1] + 1
+    memo.set(i, memo.get(i - 1) + 1 || 1)
 
     for (let j = i - 1; j >= 0; j--) {
       // Extract the substring from j to i.
       const substring = s.substring(j, i)
-
       // Check if the substring exists in the dictionary.
       if (dict.has(substring)) {
         // If it exists, update dp[i] with the minimum value between the current dp[i]
         // and dp[j], which means the minimum extra characters needed.
-        dp[i] = Math.min(dp[i], dp[j])
+        memo.set(i, Math.min(memo.get(i), Math.min(memo.get(j) || 0)))
       }
     }
   }
 
   // Return the minimum extra characters needed for the entire string s.
-  return dp[n]
+  return memo.get(n)
 }
--- 1d-dp/word-break.js
+++ 1d-dp/word-break.js
@@ -1,22 +1,33 @@
-// dp memo
-// Time ?? || Space ??
+// dp memo without recursion
+// Time O(n ^ 2) || Space O(n)
 // https://leetcode.com/problems/word-break/
 
-var wordBreak = function (s, wordDict, memo = new Map()) {
-  if (!s) return true
-  if (memo.has(s)) return memo.get(s)
+const wordBreak = function (s, wordDict) {
+  // Create a set to store the words from the dictionary for efficient lookups.
+  const dict = new Set(wordDict)
 
-  for (let word of wordDict) {
-    const isBaseCase = s.indexOf(word) === 0
+  // Get the length of the input string.
+  const n = s.length
 
-    if (isBaseCase) {
-      if (wordBreak(s.slice(word.length), wordDict, memo)) {
-        memo.set(s, true)
-        return true
+  // Initialize an array to store the minimum extra characters needed for substrings of s.
+  const memo = new Map()
+
+  for (let i = 1; i <= n; i++) {
+    // Start by assuming that adding one character is needed.
+    memo.set(i, memo.get(i - 1) + 1 || 1)
+
+    for (let j = i - 1; j >= 0; j--) {
+      // Extract the substring from j to i.
+      const substring = s.substring(j, i)
+      // Check if the substring exists in the dictionary.
+      if (dict.has(substring)) {
+        // If it exists, update dp[i] with the minimum value between the current dp[i]
+        // and dp[j], which means the minimum extra characters needed.
+        memo.set(i, Math.min(memo.get(i), Math.min(memo.get(j) || 0)))
       }
     }
   }
 
-  memo.set(s, false)
-  return false
+  // Return the minimum extra characters needed for the entire string s.
+  return memo.get(n) === 0 ? true : false
 }
