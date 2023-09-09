2d325633860fe7e086cff92148a1fb4e21aa7271
KIRILL F <tiltlessman@gmail.com>
2023-09-09 11:17:53 +0300

    [refresh] house-robber syntax changes

--- 1d-dp/house-robber.js
+++ 1d-dp/house-robber.js
@@ -3,21 +3,28 @@
 // Time O(N) | Space O(N)
 // https://leetcode.com/problems/house-robber/
 
-const rob = function(nums) {
-    const memo = new Map();
-    
-    const recursive = (nums, i) => {
-        const isBaseCase = i < 0
-        if(isBaseCase) return 0
-        
-        if(memo.has(i)) return memo.get(i)
-        
-        const oneStep = recursive(nums, i - 1) 
-        const twoSteps = recursive(nums, i - 2) + nums[i]
-        memo.set(i, memo.get(i) + Math.max(oneStep, twoSteps) || Math.max(oneStep, twoSteps))
-        
-        return memo.get(i)
-    }
-    
-    return recursive(nums, nums.length - 1)
-};
+const rob = function (nums) {
+  const memo = new Map()
+
+  const recursive = (index) => {
+    const isBaseCase = nums.length <= index
+    if (isBaseCase) return 0
+
+    if (memo.has(index)) return memo.get(index)
+
+    const leftHouse = nums[index]
+    const midHouse = recursive(index + 1)
+    const rightHouse = recursive(index + 2)
+    const twoHouses = leftHouse + rightHouse
+
+    memo.set(
+      index,
+      memo.get(index) + Math.max(twoHouses, midHouse) ||
+        Math.max(twoHouses, midHouse)
+    )
+
+    return memo.get(index)
+  }
+
+  return recursive(0)
+}
