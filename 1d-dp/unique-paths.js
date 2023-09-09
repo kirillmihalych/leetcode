d4075c1f19dc717303796d72a35cbfe3a0bb9a7c
KIRILL F <tiltlessman@gmail.com>
2023-09-09 11:01:50 +0300

    [refresh] unique-paths little changes

--- 1d-dp/unique-paths.js
+++ 1d-dp/unique-paths.js
@@ -3,25 +3,24 @@
 // https://leetcode.com/problems/unique-paths/
 
 const uniquePaths = function (m, n) {
-  let memo = getMemo(m, n)
+  const memo = getMemo(m, n)
 
-  const recursive = (column, row) => {
-    const isBaseCase = column === 1 || row === 1
+  const recursive = (col, row) => {
+    const isBaseCase = col === 1 || row === 1
     if (isBaseCase) return 1
 
-    const hasSeen = memo[row][column] !== 0
-    if (hasSeen) return memo[row][column]
+    const hasSeen = memo[col][row] !== 0
+    if (hasSeen) return memo[col][row]
 
-    const down = recursive(column - 1, row)
-    const right = recursive(column, row - 1)
+    const down = recursive(col, row - 1)
+    const right = recursive(col - 1, row)
+    memo[col][row] = down + right
 
-    memo[row][column] = down + right
-
-    return memo[row][column]
+    return memo[col][row]
   }
 
-  return recursive(n, m)
+  return recursive(m, n)
 }
 
-const getMemo = (row, col) =>
-  new Array(row + 1).fill().map(() => new Array(col + 1).fill(0))
+const getMemo = (col, row) =>
+  new Array(col + 1).fill().map(() => new Array(row + 1).fill(0))
