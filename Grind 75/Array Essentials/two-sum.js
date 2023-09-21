c3444244b3f12f71dfc8c4824f0414d88844281f
KIRILL F <tiltlessman@gmail.com>
2023-09-21 10:54:42 +0300

    [grind 75] hash table essentials

--- Grind 75/Array Essentials/two-sum.js
+++ Grind 75/Array Essentials/two-sum.js
@@ -1,27 +1,18 @@
-// hash map solution
-// Time O(n) || Space O(n)
+// Array
+// Time O(N^2) || Space O(n)
 // https://leetcode.com/problems/two-sum/
 
 const twoSum = function (nums, target) {
   const result = []
-  const map = getMap(nums)
-
-  for (const num of nums) {
-    const key = target - num
-
-    if (map.has(key) && map.get(key) !== nums.indexOf(num)) {
-      result.push(map.get(key), nums.indexOf(num))
-      return result
-    }
-  }
-}
-
-const getMap = (nums) => {
-  const map = new Map()
 
   for (let i = 0; i < nums.length; i++) {
-    map.set(nums[i], i)
-  }
+    const key = target - nums[i]
 
-  return map
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] === key && j !== i) {
+        result.push(j, i)
+        return result
+      }
+    }
+  }
 }
---
+++ Grind 75/Hash table essentials/two-sum.js
@@ -0,0 +1,27 @@
+// hash
+// Time O(n) || Space O(n)
+// https://leetcode.com/problems/two-sum/
+
+const twoSum = function (nums, target) {
+  const result = []
+  const map = getMap(nums)
+
+  for (const num of nums) {
+    const key = target - num
+
+    if (map.has(key) && map.get(key) !== nums.indexOf(num)) {
+      result.push(map.get(key), nums.indexOf(num))
+      return result
+    }
+  }
+}
+
+const getMap = (nums) => {
+  const map = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i)
+  }
+
+  return map
+}
