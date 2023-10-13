27a14f32dca375db2bccb26f2ddf50a2e7455dbc
KIRILL F <tiltlessman@gmail.com>
2023-10-13 17:37:22 +0300

    [DCC October 2023] October 13

---
+++ DCC October 2023/min-most-climbing-stairs.js
@@ -0,0 +1,13 @@
+const minCostClimbingStairs = (cost, index = cost.length, memo = new Map()) => {
+  const isBaseCase = index <= 1
+  if (isBaseCase) return 0
+
+  if (memo.has(index)) return memo.get(index)
+
+  const [prev, prevPrev] = [index - 1, index - 2]
+  const left = minCostClimbingStairs(cost, prev, memo) + cost[prev]
+  const right = minCostClimbingStairs(cost, prevPrev, memo) + cost[prevPrev]
+
+  memo.set(index, Math.min(left, right))
+  return memo.get(index)
+}
