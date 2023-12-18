fab9adb5574da3a387e1544eece37833d493f417
KIRILL F <tiltlessman@gmail.com>
2023-12-18 21:38:07 +0300

    daily

---
+++ DCC October 2023/max-prod-diff.js
@@ -0,0 +1,10 @@
+var maxProductDifference = (nums) => {
+  let maxArr = []
+  let minArr = []
+  let sortedArr = nums.sort((a, b) => a - b)
+  for (let i = 0; i < 2; i++) {
+    maxArr.push(sortedArr.pop())
+    minArr.push(sortedArr.shift())
+  }
+  return maxArr[0] * maxArr[1] - minArr[0] * minArr[1]
+}
