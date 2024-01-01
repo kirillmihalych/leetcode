11f94c52b8dbf7a34aedf5afdbe260bc0300bee8
KIRILL F <tiltlessman@gmail.com>
2024-01-01 20:48:39 +0300

    daily

---
+++ DCC October 2023/find-content.js
@@ -0,0 +1,13 @@
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+  let res = 0
+  for (let i = 0, j = 0; i < g.length && j < s.length; i++, j++) {
+    if (g[i] <= s[j]) {
+      res++
+    } else {
+      i--
+    }
+  }
+  return res
+}
