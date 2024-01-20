45b1176ac02ab92b201da83e679bcb87b8aa515c
KIRILL F <tiltlessman@gmail.com>
2024-01-20 21:37:24 +0300

    daily

---
+++ DCC October 2023/numbers-added.js
@@ -0,0 +1,20 @@
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0
+  let resultNode = new ListNode(0)
+  let returnNode = resultNode
+
+  while (l1 || l2) {
+    const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry
+    resultNode.next = new ListNode(sum >= 10 ? sum % 10 : sum)
+    carry = sum >= 10 ? Math.floor(sum / 10) : 0
+    l1 = l1 ? l1.next : null
+    l2 = l2 ? l2.next : null
+    resultNode = resultNode.next
+  }
+
+  if (carry > 0) {
+    resultNode.next = new ListNode(carry)
+  }
+
+  return returnNode.next
+}
