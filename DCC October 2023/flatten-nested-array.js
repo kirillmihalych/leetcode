48b39fbaf73b45fa9cf2c400a3893b0174ef4bec
KIRILL F <tiltlessman@gmail.com>
2024-01-25 20:43:47 +0300

    daily

---
+++ DCC October 2023/flatten-nested-array.js
@@ -0,0 +1,16 @@
+var flat = function (arr, n) {
+  const res = []
+
+  function helper(arr, depth) {
+    for (const val of arr) {
+      if (typeof val === 'object' && depth < n) {
+        helper(val, depth + 1)
+      } else {
+        res.push(val)
+      }
+    }
+    return res
+  }
+
+  return helper(arr, 0)
+}
